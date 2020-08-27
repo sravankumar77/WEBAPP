4451ff6592b9f857e86205a31e58e3d9e062fd27
indianche <durgamsanthosh141@gmail.com>
2020-08-27 13:06:34 +0530

    everything

---
+++ src/main/java/App.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public int add(int a, int b){
+
+           return a+b;
+     }
+
+    public int multiply(int a, int b){
+
+             return a*b;
+     }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    
+}
