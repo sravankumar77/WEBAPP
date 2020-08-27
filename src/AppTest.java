83cbe691ee855019bbe24d9a89818ddc218f44f9
Santhosh Durgam <durgamsanthosh141@gmail.com>
2020-08-27 12:53:56 +0530

    Add files via upload

---
+++ src/AppTest.java
@@ -0,0 +1,44 @@
+package com.santhosh;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+
+public class AppTest 
+{
+     private App myapp;
+
+     @Before
+      public void setUp()throws Exception{
+	  myapp=new App();
+
+      }
+
+      @After
+       public void tearDown()throws Exception{
+             
+	}
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+  
+      @Test
+      public void testAdd(){
+            int result=myapp.add(2,3);
+             assertEquals(result,5);
+            
+	}
+
+      @Test
+      public void testMultiply(){
+            int result=myapp.multiply(2,3);
+             assertEquals(result,6);
+            
+	}
+}
